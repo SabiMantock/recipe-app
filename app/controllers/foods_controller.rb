1e88d87e39fc9aa2197ce92d86d530cfaac3a4c8
SabiMantock <sabimantock@gmail.com>
2023-07-03 10:52:19 +0000

    Add Foods controller Add routes for foods Add views

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,26 @@
+class FoodsController < ApplicationController
+    # before_action :authenticate_user!
+
+  def index
+    @foods = Food.all
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to foods_path, notice: 'Food was successfully added.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :foods
+  accepts_nested_attributes_for :foods
 end
