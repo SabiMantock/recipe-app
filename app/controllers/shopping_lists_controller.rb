9bb9fe06ebdb497c8a40386936b0715f6ed58aae
SabiMantock <sabimantock@gmail.com>
2023-07-04 13:13:36 +0000

    feat: add general shopping list, shoppinglist controllers, routes and view

---
+++ app/controllers/shopping_lists_controller.rb
@@ -0,0 +1,22 @@
+class ShoppingListsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @recipes = current_user.recipes.includes(:recipe_foods)
+    @missing_foods = find_missing_foods
+    @total_items = @missing_foods.sum(:quantity)
+    @total_price = calculate_total_price(@missing_foods)
+  end
+
+  private
+
+  def find_missing_foods
+    user_foods = current_user.foods
+    # recipe_foods = RecipeFood.includes(:food).where(recipe: @recipes).group(:food_id)
+    # recipe_foods.select { |rf| user_foods.find_by(id: rf.food_id).nil? }
+  end
+
+  def calculate_total_price(foods)
+    foods.sum { |food| food.food.price * food.quantity }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       authenticated :user do
     root 'foods#index', as: :authenticated_root
     resources :foods, only: [:index, :new, :create]
+    resources :shopping_lists, only: [:index]
+
       end
     root 'recipes#index'
   resources :recipes, only: [:index]
