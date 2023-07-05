673478e26d9288f3a6c42a51e93e75b50a255339
SaneMyburg <sane.myburg@gmail.com>
2023-07-05 11:27:31 +0200

    Fix robocop

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: %i[index show]
 
   def index
     @recipes = current_user.recipes
