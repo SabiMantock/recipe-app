5364daaf3077ebf3215b90e21ac1a55eb21d456a
SaneMyburg <sane.myburg@gmail.com>
2023-07-05 05:27:17 +0200

    Add actions in recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,38 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @recipes = current_user.recipes
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = current_user.recipes.build
+  end
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+    puts recipe_params
+    if @recipe.save
+      redirect_to recipe_path(@recipe), notice: "Recipe created successfully."
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @recipe = current_user.recipes.find(params[:id])
+    puts "Destroying recipe with ID: #{params[:id]}"
+    @recipe.destroy
+    redirect_to recipes_path, notice: "Recipe deleted successfully."
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :public, :description)
   end
 end
