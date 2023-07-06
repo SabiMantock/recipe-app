82994b8b073b2fd2c6106f9e19f1dd5217fa7379
SabiMantock <sabimantock@gmail.com>
2023-07-06 13:32:02 +0000

    fix: linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -58,12 +58,12 @@ class RecipesController < ApplicationController
     redirect_to recipes_path, notice: 'Recipe deleted successfully.'
   end
 
-def shopping_list
-  @recipes = current_user.recipes.includes(:recipe_foods)
-  @missing_foods = find_missing_foods
-@total_items = @missing_foods.pluck(:quantity).sum
-  @total_price = calculate_total_price(@missing_foods)
-end
+  def shopping_list
+    @recipes = current_user.recipes.includes(:recipe_foods)
+    @missing_foods = find_missing_foods
+    @total_items = @missing_foods.pluck(:quantity).sum
+    @total_price = calculate_total_price(@missing_foods)
+  end
 
   private
 
@@ -79,22 +79,22 @@ end
     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :public, :description)
   end
 
-def find_missing_foods
-  missing_foods = []
-  @recipes.each do |recipe|
-    recipe.recipe_foods.each do |recipe_food|
-      missing_quantity = recipe_food.quantity
-      missing_foods << recipe_food if missing_quantity && missing_quantity > 0
+  def find_missing_foods
+    missing_foods = []
+    @recipes.each do |recipe|
+      recipe.recipe_foods.each do |recipe_food|
+        missing_quantity = recipe_food.quantity
+        missing_foods << recipe_food if missing_quantity&.positive?
+      end
     end
+    missing_foods
   end
-  missing_foods
-end
 
-def calculate_total_price(missing_foods)
-  total_price = 0
-  missing_foods.each do |recipe_food|
-    total_price += recipe_food.quantity * recipe_food.food.price
+  def calculate_total_price(missing_foods)
+    total_price = 0
+    missing_foods.each do |recipe_food|
+      total_price += recipe_food.quantity * recipe_food.food.price
+    end
+    total_price
   end
-  total_price
-end
 end
