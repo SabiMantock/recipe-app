0d8c17307c382f548bcca76370f7514d484f0212
SaneMyburg <sane.myburg@gmail.com>
2023-07-06 10:47:09 +0200

    Update views to set private as default

--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_04_192404) do
     t.string "name"
     t.integer "preparation_time"
     t.integer "cooking_time"
-    t.boolean "public"
+    t.boolean "public", default: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
