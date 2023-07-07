69f7eac72d2c5dd3d88078be797b1adb80a54715
SaneMyburg <sane.myburg@gmail.com>
2023-07-07 10:06:37 +0200

    Add recipes feature test

---
+++ spec/features/recipes_spec.rb
@@ -0,0 +1,121 @@
+require 'rails_helper'
+
+RSpec.describe 'Recipes index', type: :feature do
+  before(:each) do
+    @user = User.create(email: 'user@example.com', password: 'password', name: 'user')
+    @user.confirm
+    @recipe = Recipe.create(name: 'My recipe', preparation_time: 10, cooking_time: 10, description: 'test',
+                            public: false, user: @user)
+    @food = Food.create(name: 'My food', measurement_unit: 'test', price: 10, quantity: 10, user: @user)
+    sign_in @user
+    visit recipes_path
+  end
+  it 'shows the header title' do
+    expect(page).to have_content("Recipes")
+  end
+
+  it 'shows the recipe name' do
+    expect(page).to have_content(@recipe.name)
+  end
+
+  it 'shows the recipe description' do
+    expect(page).to have_content(@recipe.description)
+  end
+
+  it 'shows and visits the link to the recipe show page' do
+    expect(page).to have_link(@recipe.name)
+    click_link @recipe.name
+    expect(page).to have_current_path(recipe_path(@recipe))
+  end
+
+  it 'shows and visits the link to the add recipe page' do
+    expect(page).to have_link('Create New Recipe')
+    click_link 'Create New Recipe'
+    expect(page).to have_current_path(new_recipe_path)
+  end
+end
+
+RSpec.describe 'recipes visit index and delete recipe', type: :feature do
+
+  before(:each) do
+    @user = User.create(email: 'user@example.com', password: 'password', name: 'user')
+    @user.confirm
+    @recipe = Recipe.create(name: 'My recipe', preparation_time: 10, cooking_time: 10, description: 'test',
+                            public: false, user: @user)
+    @food = Food.create(name: 'My food', measurement_unit: 'test', price: 10, quantity: 10, user: @user)
+    sign_in @user
+    visit recipes_path
+  end
+  context 'when deleting a recipe' do
+    it 'shows the button to delete the recipe' do
+      expect(page).to have_button('Delete Recipe')
+    end
+
+    it 'deletes the recipe when the delete button is clicked' do
+      click_button 'Delete Recipe'
+      expect(page).to have_current_path(recipes_path)
+      expect(page).to have_content('No recipes found')
+      expect(page).not_to have_content(@recipe.name)
+      expect(page).not_to have_content(@recipe.description)
+      expect(page).not_to have_link(@recipe.name)
+      expect(page).not_to have_button('Delete Recipe')
+    end
+  end
+end
+
+RSpec.describe 'recipes visit show recipe ', type: :feature do
+
+  before(:each) do
+    @user = User.create(email: 'user@example.com', password: 'password', name: 'user')
+    @user.confirm
+    @recipe = Recipe.create(name: 'My recipe', preparation_time: 7, cooking_time: 10, description: 'test',
+                            public: false, user: @user)
+    @food = Food.create(name: 'My food', measurement_unit: 'test', price: 3, user: @user)
+    @recipe_food = @recipe.recipe_foods.create(quantity: 5, food: @food)
+    sign_in @user
+    visit recipe_path(@recipe)
+  end
+
+  it 'shows the recipe details' do
+    expect(page).to have_content(@recipe.name)
+    expect(page).to have_content(@recipe.description)
+    expect(page).to have_content(@recipe.preparation_time)
+    expect(page).to have_content(@recipe.cooking_time)
+  end
+
+  it 'shows the button to generate shopping list and add ingredient' do
+    expect(page).to have_link('Generate Shopping List')
+    expect(page).to have_link('Add Ingredient')
+  end
+
+  it 'shows the ingredient list details' do
+    expect(page).to have_content(@food.name)
+    expect(page).to have_content("#{@recipe_food.quantity} #{@food.measurement_unit}")
+    expect(page).to have_content(" $#{@food.price * @recipe_food.quantity}")
+    expect(page).to have_link('Modify')
+    expect(page).to have_button('Remove')
+  end
+end
+
+RSpec.describe 'recipes visit show recipe', type: :feature do
+  before(:each) do
+    @user = User.create(email: 'user@example.com', password: 'password', name: 'user')
+    @user.confirm
+    @recipe = Recipe.create(name: 'My recipe', preparation_time: 7, cooking_time: 10, description: 'test',
+                            public: false, user: @user)
+    sign_in @user
+    visit recipe_path(@recipe)
+  end
+
+  it 'toggles the recipe between public and private' do
+    expect(page).to have_button('Private')
+
+    click_button 'Private'
+    expect(page).to have_current_path(recipe_path(@recipe))
+    expect(page).to have_button('Public')
+
+    click_button 'Public'
+    expect(page).to have_current_path(recipe_path(@recipe))
+    expect(page).to have_button('Private')
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,10 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
+
+require 'rails_helper'
+include ActionView::Helpers::NumberHelper
+
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
@@ -11,7 +15,12 @@ require 'selenium-webdriver'
 
 RSpec.configure do |config|
   config.include Capybara::DSL
+
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end
+
+Capybara.default_driver = :selenium_chrome
+Capybara.javascript_driver = :selenium_chrome
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
